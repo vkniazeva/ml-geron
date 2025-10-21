10d89a658202d7a1e29bf3d0dbcf62522a5a6cb0
Viktoriia Kniazeva <viktoriia.kniazeva@omnevo.net>
2025-10-21 18:57:25 +0200

    Data loading, and describing

--- notes/chapter1.md
+++ notes/chapter1.md
@@ -1,13 +1,43 @@
 # Notes to the chapter 1
 
+## Building a new ML project
+
+Usually all ML projects can be built based on the following plan:
+
+1. Look at the big picture 
+   2. Business scope: task, target, application, focus
+   3. Current situation: process, costs, errors
+   4. ML model: task type, algorithm, data update, quality criteria
+2. Get the data.
+3. Discover and visualize the data to gain insights.
+4. Prepare the data for Machine Learning algorithms.
+5. Select a model and train it.
+6. Fine-tune your model.
+7. Present your solution.
+8. Launch, monitor, and maintain your system.
+
+
+## Glossary
+
+- Data snooping bias—recognizing data patterns, existing only in a concreate dataset
+and disappearing with the first check on new data.
+- 
 
 ## Useful Python insights
 
-DataFrame - the primary data structure in pandas, 
+- DataFrame—the primary data structure in pandas, 
 representing a two-dimensional table similar to 
 an Excel worksheet. It consists of rows and columns, 
 where each column can hold a different data type (e.g., 
 strings, integers, dates).
-
-head() - a method used to quickly inspect the beginning of a 
+- head()—method used to quickly inspect the beginning of a 
 DataFrame by returning the first 5 rows.
+- info()—method used to get a short summary about data (number of rows, 
+each attribute type, None values number)
+- value_counts() - method for counting non-numerical values appearance 
+- describe()—method for getting a statistical summary for numerical attributes (
+count, mean, std. min, percentiles, max)
+- hist() - matplotlit.pyplot - drawing a histogram: bin - detalization (higher -> more details),
+figsize - graph size (i.e. 20 - width, 15 - high)
+
+![histogram.png](assets/histogram.png)
--- scripts/chapter_1/housing_prices.py
+++ scripts/chapter_1/housing_prices.py
@@ -1,10 +1,11 @@
 import os
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-
-def load_housing_csv(filename="housing.csv", chapter="chapter_1"):
+def fetch_housing_data(filename, chapter):
     """
-    Load housing data from assets folder.
+    Load housing data from the assets folder.
 
     Parameters:
     - filename: str, name of the CSV file
@@ -31,10 +32,41 @@ def load_housing_csv(filename="housing.csv", chapter="chapter_1"):
     except Exception as e:
         raise RuntimeError(f"Unexpected error reading {csv_path}: {e}")
 
+def get_data_info(dataset):
+    """
+    Prints short information for the given dataset
+
+    Parameters:
+    - dataset: DataFrame, dataset to investigate
+    """
+    print(dataset.head())
+    print("\n" + "="*80)
+    print(dataset.info())
+    print("\n" + "=" * 80)
+    # checking not number column
+    print(dataset["ocean_proximity"].value_counts())
+    print("\n" + "=" * 80)
+    # getting description for numerical columns
+    print(dataset.describe())
+    dataset.hist(bins=50, figsize=(20,15))
+    # plt.show()
+
+def split_train_test(dataset, test_ratio):
+    shuffled_indices = np.random.permutation(len(dataset))
+    test_set_size = int(len(dataset) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return dataset.iloc[train_indices], dataset.iloc[test_indices]
+
+
 
 if __name__ == "__main__":
+    housing_filename = "housing.csv"
+    housing_chapter = "chapter_1"
     try:
-        housing = load_housing_csv()
-        print(housing.head())
+        housing = fetch_housing_data(housing_filename, housing_chapter)
+        get_data_info(housing)
     except Exception as e:
         print(e)
+
+
